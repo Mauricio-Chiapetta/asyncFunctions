d9195c41518eefcecab9419689c10205025f80f8
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-08-08 17:58:34 -0300

    final commit

--- index.js
+++ index.js
@@ -15,12 +15,97 @@
 //     console.log(results)
 // })
 
-const numbers = [4, 9, 5, 13, 77]
+// const numbers = [4, 9, 5, 13, 77]
 
-async function asyncSquare(x) {
-    return x * x
+// async function asyncSquare(x) {
+//     return x * x
+// }
+
+// Promise.all(numbers.map(number => asyncSquare(number))).then(squares => {
+//     console.log(squares)
+// })
+
+// async function soma(a,b) {
+//     if(isNaN(a)||isNaN(b)){
+//         return Promise.reject('params must be a number!')
+//     }
+//     return a + b
+// }
+
+// async function subtracao(a,b){
+//     if(isNaN(a)||isNaN(b)){
+//         return Promise.reject('params must be a number !')
+//     }
+
+//     return a - b
+// }
+
+// soma(1,null).then((result)=>{
+//     console.log(`resultado : ${result}`)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+// subtracao("rola",1).then((result)=>{
+//     console.log(`resultado: ${result}`)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+// UTILIZANDO AWAIT
+
+// async function soma(a,b) {
+//     if(isNaN(a)||isNaN(b)){
+//         return Promise.reject('os parametros devem ser numeros!')
+//     }
+//     return a + b
+// }
+
+// async function execute(){
+//     const somaResult = await soma(1,2)
+//     console.log(somaResult)
+// }
+
+// utilizando try catch e await
+
+// async function execute(){
+//     try{
+//         const somaResult = await soma(2,"ola")
+//         console.log(somaResult)
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
+
+// execute()
+
+// PROMISE ALL COM ASYNC E AWAIT
+
+function waitfor(seconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, seconds * 1000);
+    });
 }
 
-Promise.all(numbers.map(number => asyncSquare(number))).then(squares => {
-    console.log(squares)
-})
+const numbers = [1, 2, 3, 4, 5];
+
+// Promise.all((squares)).then((results)=>{
+//     console.log(results)
+// })
+
+// console.log(squares)
+
+async function execute() {
+    console.time('tempo')
+    const squares = await Promise.all(
+        numbers.map(async (number) => {
+            await waitfor(2);
+            return number * number;
+        }))
+console.log(squares)
+console.timeEnd('tempo')    
+}
+
+execute()
