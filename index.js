df93893803051536393a814e653a574ea8a34a19
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-08-09 16:52:21 -0300

    exercicio-final(refatorando promises)

--- index.js
+++ index.js
@@ -81,31 +81,94 @@
 
 // PROMISE ALL COM ASYNC E AWAIT
 
-function waitfor(seconds) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, seconds * 1000);
-    });
-}
+// function waitfor(seconds) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve();
+//         }, seconds * 1000);
+//     });
+// }
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-// Promise.all((squares)).then((results)=>{
-//     console.log(results)
-// })
+// // Promise.all((squares)).then((results)=>{
+// //     console.log(results)
+// // })
 
+// // console.log(squares)
+
+// async function execute() {
+//     console.time('tempo')
+//     const squares = await Promise.all(
+//         numbers.map(async (number) => {
+//             await waitfor(2);
+//             return number * number;
+//         }))
 // console.log(squares)
+// console.timeEnd('tempo')
+// }
 
-async function execute() {
-    console.time('tempo')
-    const squares = await Promise.all(
-        numbers.map(async (number) => {
-            await waitfor(2);
-            return number * number;
-        }))
-console.log(squares)
-console.timeEnd('tempo')    
+// execute()
+
+// EXERCICIO REFATORANDO AS PROMISES (EXERCICIO 24)
+
+// EXERCICIO - treinando o uso de promises
+// IMC = peso/(altura x altura)
+
+async function imc(peso, altura) {
+    if (isNaN(peso) || isNaN(altura)) {
+        return Promise.reject("Os parâmetros não são números!");
+    }
+    return peso / (altura * altura);
+
 }
 
-execute()
+async function exibeImc(peso, altura) {
+    try {
+        console.log(`Calculando o IMC para peso ${peso} e altura ${altura}`);
+        const calcResult = await imc(peso, altura)
+        console.log(`O resultado do IMC foi de ${calcResult}`)
+
+        if (calcResult < 18.5) {
+            console.log("magreza");
+        } else if (calcResult < 25) {
+            console.log("normal");
+        } else if (calcResult < 30) {
+            console.log("sobrepeso");
+        } else if (calcResult < 40) {
+            console.log("obesidade");
+        } else {
+            console.log("OBESIDADE GRAVE");
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+
+
+
+    // .then((result) => {
+    //   console.log(`O resultado do calculo foi: ${result}`);
+    //   if (result < 18.5) {
+    //     console.log("magreza");
+    //   } else if (result < 25) {
+    //     console.log("normal");
+    //   } else if (result < 30) {
+    //     console.log("sobrepeso");
+    //   } else if (result < 40) {
+    //     console.log("obesidade");
+    //   } else {
+    //     console.log("OBESIDADE GRAVE");
+    //   }
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+
+}
+
+exibeImc(71, 1.74);
+exibeImc(48, 1.6);
+exibeImc(71, "texto");
+exibeImc(82, 1.72);
+exibeImc(120, 1.8);
